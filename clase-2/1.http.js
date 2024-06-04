28ea0b9d3f315ea38df81b02920a00fa83d57724
Bruno P <brunopalavecino62@gmail.com>
2024-06-03 21:05:10 -0300

    Add methods GET and POST for API

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,11 +1,22 @@
 const http = require('node:http') //protocolo http
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     if (req.url === '/') {
-        res.end('<h1>Bienvenido a mi página de inicio</h1>')
+        res.end('<h1>Bienvenido a mi página de iniciooooooooooooo</h1>')
+    } else if (req.url === '/imagen-del-chipi.png') {
+        fs.readFile('./chip.png', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1>500 Internal Server Error</h1>')
+            } else {
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     } else if (req.url === '/contacto') {
         res.end('<h1>Contacto</h1>')
     } else {
